0af30b29d6ca3bad31ef6a779a49b31ee41bddda
GuillaumeGas <guillaume@gas-ntic.fr>
2020-12-27 16:18:50 +0100

    maj

--- model/affectation_tenue.py
+++ model/affectation_tenue.py
@@ -7,9 +7,9 @@ class AffectationTenue(models.Model):
 	_name = 'croixrouge.affectation_tenue'
 	
 	benevole_id = fields.Many2one('croixrouge.benevole', string="Bénévole", required=True, ondelete='set null')
-	type_tenue_id = fields.Many2one('croixrouge.type_tenue', string="Type de tenue", required=True, ondelete='set null')
+	vetement_ids = fields.Many2many('croixrouge.vetement', string="Vêtements")
 	
-	mission_id = fields.Many2one('croixrouge.mission_id', string="Mission", required=False, ondelete='set null')
+	mission_id = fields.Many2one('croixrouge.mission', string="Mission", required=False, ondelete='set null')
 	date_retour = fields.Date(string="Date de retour", required=True, default=fields.Date.today())
 	est_en_retard = fields.Boolean(compute="_est_en_retard", default=False)
 	
@@ -21,23 +21,17 @@ class AffectationTenue(models.Model):
 			if date_retour < date_courante:
 				affectation.est_en_retard = True
 			else:
-				affectation.est_en_retard = False
+				affectation.est_en_retard = False				
 				
 	@api.model
 	def create(self, values):
-		etat_disponible = 0
 		etat_affecte = 1
 		
-		type_tenue = self.env['croixrouge.type_tenue'].search([('id', '=', values['type_tenue_id'])])
-		for type_vetement_id in type_tenue.type_vetement_ids:
-			vetement_dispo = self.env['croixrouge.vetement'].search(['&', ('type_id', '=', type_vetement_id.id), ('etat', '=', etat_disponible)])
-			if (len(vetement_dispo) == 0):
-				vetement = self.env['croixrouge.type_vetement'].search([('id', '=', type_vetement_id.id)])
-				raise exceptions.ValidationError("Pas assez de " + vetement.name + " en stock")
-				
-		for type_vetement_id in type_tenue.type_vetement_ids:
-			vetement_dispo = self.env['croixrouge.vetement'].search(['&', ('type_id', '=', type_vetement_id.id), ('etat', '=', etat_disponible)])
-			vetement_dispo[0].write({'etat': etat_affecte})
+		for entry in values['vetement_ids']:
+			(a, b, vetement_ids) = entry
+			for vetement_id in vetement_ids:
+				vetement = self.env['croixrouge.vetement'].search([('id', '=', vetement_id)])
+				vetement[0].write({'etat': etat_affecte})
 				
 		res = super(AffectationTenue, self).create(values)
 		return res
@@ -46,12 +40,8 @@ class AffectationTenue(models.Model):
 	def unlink(self):
 		for affectation in self:
 			etat_disponible = 0
-			etat_affecte = 1
 		
-			type_tenue = self.env['croixrouge.type_tenue'].search([('id', '=', affectation.type_tenue_id.id)])	
-			for type_vetement_id in type_tenue.type_vetement_ids:
-				vetement_affecte = self.env['croixrouge.vetement'].search(['&', ('type_id', '=', type_vetement_id.id), ('etat', '=', etat_affecte)])
-				if len(vetement_affecte) > 0:
-					vetement_affecte[0].write({'etat': etat_disponible})
+			for vetement in affectation.vetement_ids:
+				vetement.write({'etat': etat_disponible})
 			
 			return super(AffectationTenue, self).unlink()
--- model/benevole.py
+++ model/benevole.py
@@ -4,7 +4,7 @@ from openerp import api, models, fields
 class Benevole(models.Model):
 	_name = 'croixrouge.benevole'
 	
-	nom = fields.Char(string="Nom", required=True)
+	name = fields.Char(string="Nom", required=True)
 	prenom = fields.Char(string="Prenom", required=True)
 	nivol = fields.Char(string="Nivol", required=True)
 	
